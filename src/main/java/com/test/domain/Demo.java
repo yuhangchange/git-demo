f000bc198f0f6e1b6658a70b04185306d2c6f532
wufan <1210799018@qq.com>
2021-11-02 15:14:49 +0800

    test01测试

---
+++ src/main/java/com/test/domain/Demo.java
@@ -0,0 +1,11 @@
+package com.test.domain;
+
+public class Demo {
+    public static void main(String[] args) {
+        test1();
+    }
+
+    private static void test1(){
+        System.out.println("test1");
+    }
+}
